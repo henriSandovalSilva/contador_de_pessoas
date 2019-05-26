3ae67637ee24d251aebe5bbede1e6d3b89494e1a
henriSandovalSilva <42452534+henriSandovalSilva@users.noreply.github.com>
2019-05-26 01:22:57 -0300

    Adionada a ação dos botões

--- lib/main.dart
+++ lib/main.dart
@@ -4,60 +4,105 @@ void main() {
   runApp(
     new MaterialApp(
         title: 'Contador de pessoas',
-        //Home do App - Column
-        home: Column(
+        //Home do App
+        home: Home()),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //Quantidade total de pessoas que entraram
+  int _pessoas = 0;
+
+  //Status do local
+  String _status = "Vazio!";
+
+  void _atualiza(int valor) {
+    setState(() {
+      //Atualiza o valor conforme o clique do botão "+1" e "-1"
+      _pessoas += valor;
+
+      //Atualiza o valor conforme a quantidade de pessoas
+      if (_pessoas < 10 && _pessoas > 0) {
+        _status = "Disponível!";
+      } else if (_pessoas < 1) {
+        _status = "Vazio!";
+      } else {
+        _status = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //Alinhamento da coluna na tela
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        //Texto com a quantidade de pessoas
+        Text(
+          "Pessoas: $_pessoas",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        //Linha com os botões
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          //Alinhamento da coluna na tela
+          //Alinhamento da linha na tela
           children: <Widget>[
-            //Texto
-            Text(
-              "Pessoas: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            //Linha com os botões
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              //Alinhamento da linha na tela
-              children: <Widget>[
-                //Padding do botão
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  //Botão
-                  child: FlatButton(
-                    onPressed: () {}, //Quando clicar no botão
-                    //Texto dentro do botão
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ),
-                  ),
+            //Padding do botão
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //Botão
+              child: FlatButton(
+                //Clique do botão
+                onPressed: () {
+                  //Atualiza o valor total de pessoas
+                  if (_pessoas < 10) {
+                    _atualiza(1);
+                  }
+                },
+                //Texto dentro do botão
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
                 ),
-                //Padding do botão
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  //Botão
-                  child: FlatButton(
-                    onPressed: () {}, //Quando clicar no botão
-                    //Texto dentro do botão
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-            //Texto
-            Text(
-              "Pode entrar!",
-              style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0,
+            //Padding do botão
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //Botão
+              child: FlatButton(
+                //Clique do botão
+                onPressed: () {
+                  //Atualiza o valor total de pessoas
+                  if (_pessoas > 0) {
+                    _atualiza(-1);
+                  }
+                },
+                //Texto dentro do botão
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
               ),
             ),
           ],
-        )),
-  );
+        ),
+        //Texto com status do local
+        Text(
+          _status,
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0,
+          ),
+        ),
+      ],
+    );
+  }
 }
