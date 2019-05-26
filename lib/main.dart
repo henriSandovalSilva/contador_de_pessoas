5c0a08854fd60ad13bb89876a32582ce6a31af13
henriSandovalSilva <42452534+henriSandovalSilva@users.noreply.github.com>
2019-05-26 00:11:13 -0300

    Adicionado primeiro Column e Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    new MaterialApp(
+        title: 'Contador de pessoas',
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Pode entrar!",
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ),
+            ),
+          ],
+        )),
+  );
 }
